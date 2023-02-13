f22a66ffb28f8a4cec84d8e4ddc2544e69fc61cb
Andrey Iljasov <a_iljasov@mail.ru>
2023-02-13 23:30:37 +0300

    port 8085

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             var engine = new BooleanSearchEngine(new File("pdfs"));
-            var server = new Server(8989, engine);
+            var server = new Server(8085, engine);
             server.start();
         } catch (IOException e) {
             System.out.println("Ошибка создания движка поиска.");
